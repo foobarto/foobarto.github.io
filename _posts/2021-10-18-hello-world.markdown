361d34b7c5cc8789ff4058bc2735d5244f232a08
Bartosz Ptaszynski <foobarto@gmail.com>
2021-10-18 03:23:33 +0100

    hello world

---
+++ _posts/2021-10-18-hello-world.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title: "Hello World"
+date: 2021-10-18 03:20:00 +0100
+categories: deleteme
+---
+Hello World
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Hello World  ","categories": ["deleteme"],
+        "tags": [],
+        "url": "/deleteme/2021/10/18/hello-world.html",
+        "teaser": null
+      }]
