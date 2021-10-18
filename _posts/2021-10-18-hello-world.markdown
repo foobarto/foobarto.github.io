92355e08155ccb7078c8303111166e982a78e94c
Bartosz Ptaszynski <foobarto@gmail.com>
2021-10-18 04:13:04 +0100

    correcting layout

--- _posts/2021-10-18-hello-world.markdown
+++ _posts/2021-10-18-hello-world.markdown
@@ -1,5 +1,4 @@
 ---
-layout: post
 title: "Hello World"
 date: 2021-10-18 03:20:00 +0100
 categories: deleteme
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Hello World",
-        "excerpt":"Hello World  ","categories": ["deleteme"],
-        "tags": [],
-        "url": "/deleteme/2021/10/18/hello-world.html",
-        "teaser": null
-      }]
